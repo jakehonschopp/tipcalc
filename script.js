7efed74648065169a5f69bea7894861adc0181cb
Jake Honschopp <Jakehonschopp@yahoo.com>
2021-12-09 19:24:57 -0500

    first attempt at sending in

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+	const billAmount = document.getElementById("bill_amount").value;
+	const tipPercentage = document.getElementById("tip_percentage").value;
+
+	const tipAmount = document.getElementById("tip_amount").value = billAmount/tipPercentage; 
+	document.getElementById("total_bill").value = parseFloat(billAmount) + parseFloat(tipAmount); 
+
+
+}) 
