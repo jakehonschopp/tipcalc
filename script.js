1e1c4040729df76657537e3e431bc462009ead59
Jake Honschopp <Jakehonschopp@yahoo.com>
2021-12-14 15:45:52 -0500

    made changes after feedback

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", function() {
 	const billAmount = document.getElementById("bill_amount").value;
 	const tipPercentage = document.getElementById("tip_percentage").value;
 
-	const tipAmount = document.getElementById("tip_amount").value = billAmount/tipPercentage; 
+	const tipAmount = document.getElementById("tip_amount").value = billAmount x (tipPercentage / 100); 
 	document.getElementById("total_bill").value = parseFloat(billAmount) + parseFloat(tipAmount); 
 
 
