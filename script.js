7d8297cb1c46432992d853748a38fc2b49b4fa4a
Jake Honschopp <Jakehonschopp@yahoo.com>
2022-01-06 18:34:25 -0500

    fixing errors

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ btn.addEventListener("click", function() {
 	const billAmount = document.getElementById("bill_amount").value;
 	const tipPercentage = document.getElementById("tip_percentage").value;
 
-	const tipAmount = document.getElementById("tip_amount").value = billAmount x (tipPercentage / 100); 
+	const tipAmount = document.getElementById("tip_amount").value = billAmount * (tipPercentage / 100); 
 	document.getElementById("total_bill").value = parseFloat(billAmount) + parseFloat(tipAmount); 
 
 
-}) 
+});  
